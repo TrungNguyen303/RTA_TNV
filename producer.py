86a00863a8b1f58123f322493c9affc2bb93fb87
Trung Nguyen Van <trungdao.td@gmail.com>
2026-05-04 17:01:39 +0200

    Add files via upload

--- producer.py
+++ producer.py
@@ -3,7 +3,7 @@ import json, random, time
 from datetime import datetime
 
 producer = KafkaProducer(
-    bootstrap_servers='localhost:29092',
+    bootstrap_servers='host.docker.internal:29092',
     value_serializer=lambda v: json.dumps(v).encode('utf-8')
 )
 
